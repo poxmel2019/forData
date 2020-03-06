183380a4cecca11a985f69ead1e225bd64e8ee8a
Madiyar <nu_ofsn11@mail.ru>
2020-03-06 14:42:43 +0600

    read and write 3 file to one film

--- textProcessing.py
+++ textProcessing.py
@@ -2,24 +2,33 @@ import codecs
 
 
 def op1():
-    
+    global new_file
     file_addresses = ['D:\\mySpreadsheets\\прочитанное\\2016.txt',
                       'D:\\mySpreadsheets\\прочитанное\\2017.txt',
                       'D:\\mySpreadsheets\\прочитанное\\2018.txt']
 
     #file_addresses = 'D:\\mySpreadsheets\\прочитанное\\2016.txt'
 
+    new_file = open('common_list.txt','w',encoding='utf-8')
     i = 0
     for x in range(0,len(file_addresses)):
-        open_read_file(file_addresses[i])
+        open_read_file(file_addresses[i],new_file)
         i += 1
-
-def open_read_file(file_addresses):
+        
+    new_file.close()
+    
+def open_read_file(file_addresses,new_file):
     f = open(file_addresses, 'r',encoding='utf-8')
+   
     f1 = f.readlines()
+    
+    i = 0
     for x in f1:
-        if x == '\n': break
+        
+        new_file.write(x)
         print(x)
+        if x == '\n': break
+    #new_file.close()
     f.close()
 
 
