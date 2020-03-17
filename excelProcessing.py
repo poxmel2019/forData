061fc614064f6ecc8782fb7b50ea2d177b5a0dd6
Madiyar <nu_ofsn11@mail.ru>
2020-03-17 10:41:07 +0600

    The 2 script changing

--- excelProcessing.py
+++ excelProcessing.py
@@ -1,4 +1,5 @@
 from openpyxl import load_workbook
+from openpyxl import Workbook
 # Import relevant modules fomr `openpyxl.utils`
 from openpyxl.utils import get_column_letter, column_index_from_string
 
@@ -54,7 +55,106 @@ def f():
         for cell in cell_object:
             print(cell.coordinate, cell.value)
         print('--- END ---')
+
+def f1():
+    # D:\mySpreadsheets
+    global i, j, texts
+    wb = load_workbook('D:\\mySpreadsheets\\books.xlsx')
+    sheet = wb['Лист1']
+    needed_cell = sheet['C53']
+    #print(needed_cell.value)
+
+    texts = []
+    authors = []
+
+    i = 0
+    coordinates = [['C53','C55'],['D53','D55']]
+    for i in range(0,2):
+        for cell_object in sheet[coordinates[i][0]:coordinates[i][1]]:
+            for cell in cell_object:
+                if i == 0:
+                    texts.append(cell.value)
+                elif i == 1:
+                    authors.append(cell.value)
+        i += 1
+
+    print(texts)
+    print(authors)
+
+
+    file = open('D:\\pyda\\прочитанное\\2020.txt','a',encoding='utf-8')
+    #f = file.readlines()
+    file.write('\n')
+    i = 0
+    for x in range(0,len(texts)):
+        file.write(texts[i] + ' - ' + authors[i] + '\n')
+        i += 1
+    file.close()
+
+def f():
+
+    #wb = Workbook('D:\\mySpreadsheets\\books.xlsx')
+    wb = load_workbook('D:\\mySpreadsheets\\books.xlsx')
+    ws = wb.active
+    #sheet = ws['Лист1']
+    ws['C56'] = 'text'
+    ws['D56'] = 'author'
+    wb.save('D:\\mySpreadsheets\\books.xlsx')
+    ws.cell(row=2)
+def f2():
+
+    global i, j, authors_list
+    texts = open('common_text.txt','r',encoding='utf-8')
+    authors = open('common_author.txt','r',encoding='utf-8')
+    texts_list = texts.readlines()
+    authors_list = authors.readlines()
+    wb = load_workbook('D:\\mySpreadsheets\\books.xlsx')
+    ws = wb.active
+    sheet = wb['Лист1']
     
+    #print(ws.cell(row=292,column=3).value)
+    i = 4
+    for x in range(i,292):
+        ws.cell(row=i,column=3).value = texts_list[i-4]
+        i += 1
+
+    j = 4
+    for y in range(j,292):
+        ws.cell(row=j,column=4).value = authors_list[j-4]
+        j += 1
+       
+    texts.close()
+    authors.close()
+    wb.save('D:\\mySpreadsheets\\books.xlsx')
+
+def f3():
+
+    global years_list
+    year_file = open('common_year.txt','r')
+    years_list = year_file.readlines()
+
+    j = 0
+    for x in years_list:
+        years_list[j] = years_list[j][:x.index('\n')] 
+        j += 1
+
+    wb = load_workbook('D:\\mySpreadsheets\\books.xlsx')
+    sheet = wb['Лист1']
+
+    i = 4
+    for x in range(i,292):
+        #sheet.cell(row=i,column=5).value = years_list[i-4][:years_list[i-4].index('\n')]
+        sheet.cell(row=i,column=5).value = years_list[i-4]
+        i += 1
+
+    year_file.close()
+    wb.save('D:\\mySpreadsheets\\books.xlsx')
+    
+        
+
+    
+    
+        
             
     
     
--- textProcessing.py
+++ textProcessing.py
@@ -16,27 +16,41 @@ def to_handle_text():
     file.close()
 def to_handle_author():
     
-    global f, authors
+    global f, authors, years
     file_address = 'common_list.txt'
     file = open(file_address,'r',encoding='utf-8')
     new_file = open('common_author.txt','w',encoding='utf-8')
+    years_file = open('commont_year.txt','w',encoding='utf-8')
     f = file.readlines()
     i = 0
     authors = []
+    years = []
+    years_count = 0
+    year20 = '2017'
     while (i < len(f)-1):
         if (i < 130):
             authors.append(f[i][f[i].index('(')+1:f[i].rindex('')])
-            print(authors[i][0:authors[i].index(')')])
+            #print(authors[i][0:authors[i].index(')')])
             new_file.write(authors[i][0:authors[i].index(')')]+'\n')
+            years.append('2016')
+            years_file.write(years[i])
         else:
             if '(' not in f[i]:
                 f[i] = f[i] + '(f)'
             authors.append(f[i][f[i].index(' - ')+2:f[i].index('(')].strip())
-            print(authors[i][:f[i].index('(')])
+            years_count += 1
+            if years_count >= 80:
+                year20 = '2018'
+            years.append(year20)
+            years_file.write(years[i])
+            #print(authors[i][:f[i].index('(')])
             new_file.write(authors[i][:f[i].index('(')]+'\n')    
         i += 1
     file.close()
     new_file.close()
+    years_file.close()
+
+    show_list(years)
     
 def to_handle_00():
     global authors, x, texts, years, error_count, authors2
@@ -51,18 +65,21 @@ def to_handle_00():
 
     error_count = 0
     i = 0
-    for x in file_list:
-        try:texts2[i].append(x[x.index('"')+1:x.index('"')].strip())
-        except ValueError:
-            error_count += 1;
-            if error_count in (4,7,10): i += 1
-            continue
 
-    show_list(texts)
+    for x in file_list:
+        try:
+            texts2.append(x[x.index('"'):x.index('",')])
+        except ValueError:
+            try:
+                texts2.append(x[x.index('"'):x.rindex('"')])
+            except ValueError:
+                continue
+
+    show_list(texts2)
     for x in file_list:
         if x.startswith('20'): years.append(x)
 
-    show_list(years)
+    #show_list(years)
     
     i = 0
     error_count = 0
@@ -76,8 +93,63 @@ def to_handle_00():
 
     show_list(authors)
 
+def handle15():
+
+    global x, authors
+    file_address = '2015.txt'
+    handled_file = open(file_address,'r',encoding='utf-8')
+    file_list = handled_file.readlines()
+
+    authors = []
+
+    for x in file_list:
+        if x == '(303)\n':break
+        try:
+            authors.append(x[x.index(')')+1:x.index('"')].strip())
+        except ValueError:
+            continue
+        print(x)
     
+    show_list(authors)
+
+def to_handle_string():
+
+    global string, quotes_pos, txts, odd, even, j, i
+    string = '1) Антон Чехов "Который из трёх","Попрыгунья", "Хороший конец", "Один из многих", "Счастье", "Доктор", "Мститель", "Беглец", "У знакомых", "Интриги", "Ионыч"'
+    i = 0
+    quotes_pos = []
+    txts = []
+    for x in string:
+        if x == '"': quotes_pos.append(i)
+        i += 1
+    print(quotes_pos)
     
+
+    j = 0
+    odd = []
+    even = []
+    
+    for x in quotes_pos:
+        if j % 2 == 0:
+            odd.append(x)
+        else:
+            even.append(x)
+        j += 1
+
+    print(odd)
+    print(even)
+
+    j = 0
+    for x in range(0,len(odd)):
+        txts.append(string[odd[j]+1:even[j]])
+        j += 1
+
+    print(txts)
+        
+   
+        
+
+
 
 def show_list(some_list):
     for x in some_list:
